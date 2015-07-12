2abaa63cd45a4c4303c657d5925a641cf742d37e
feeman1989 <feeman1989@gmail.com>
2015-07-12 20:22:39 +0800

    new file:   init.d/zabbix_agentd new file:   init.d/zabbix_server modified:   scripts/zabbix.sh

--- scripts/zabbix.sh
+++ scripts/zabbix.sh
@@ -10,14 +10,26 @@ Install_Zabbix(){
     drop database if exists zabbix;
     create database zabbix default character set utf8;
 EOF
-    cd database/mysql 
-    mysql -uroot -p zabbix < schema.sql
-    mysql -uroot -p zabbix < images.sql
-    mysql -uroot -p zabbix < data.sql
+    mysql -uroot -pnew-password zabbix < database/mysql/schema.sql
+    mysql -uroot -pnew-password zabbix < database/mysql/images.sql
+    mysql -uroot -pnew-password zabbix < database/mysql/data.sql
     ln -sf /usr/local/zabbix/etc/ etc/zabbix
-    cp -rf frontends/php /data/webapp
-    chown -R nginx.nginx /data/webapp
-    chmod -R 755 /data/webapp
-    cp -rf $PWD_Dir/conf/zabbix.conf /usr/local/nginx/conf/
+    \cp -rf frontends/php /data/webapp/zabbix
+    chown -R nginx.nginx /data/webapp/zabbix
+    chmod -R 755 /data/webapp/zabbix
+    \cp -rf $PWD_Dir/conf/zabbix.conf /usr/local/nginx/conf/
+	\cp -rf /{zabbix_server,zabbix_agentd} /etc/init.d/
+	#sed -i "s@^ZABBIX_BIN=.*@ZABBIX_BIN=\"/usr/local/zabbix/sbin/zabbix_agentd\"@" /etc/init.d/zabbix_agentd
+	#sed -i "s@^ZABBIX_BIN=.*@ZABBIX_BIN=\"/usr/local/zabbix/sbin/zabbix_server\"@" /etc/init.d/zabbix_server
+	#sed -i "/^ZABBIX_BIN=.*/ a\ZABBIX_CONFIG=\"/usr/local/zabbix/etc/zabbix_server.conf\"" /etc/init.d/zabbix_server
+	#sed -i "/^ZABBIX_BIN=.*/ a\ZABBIX_CONFIG=\"/usr/local/zabbix/etc/zabbix_agentd.conf\"" /etc/init.d/zabbix_agentd
+	
+	echo "zabbix-agent 10050/tcp # Zabbix Agent" >> /etc/services
+	echo "zabbix-agent 10050/udp # Zabbix Agent" >> /etc/services
+	echo "zabbix-trapper 10051/tcp # Zabbix Trapper" >> /etc/services
+	echo "zabbix-trapper 10051/udp # Zabbix Trapper" >> /etc/services
+	
+	
+ 
 }
 
