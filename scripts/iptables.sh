d53cbb87178d837467d093ba41bd2494ee0efc5c
feeman1989 <543361609@qq.com>
2016-03-30 10:01:10 +0800

    Update iptables.sh

--- scripts/iptables.sh
+++ scripts/iptables.sh
@@ -10,5 +10,8 @@ $IPTABLES -t nat -F
 $IPTABLES -t filter -P INPUT DROP
 $IPTABLES -t filter -P OUTPUT ACCEPT
 $IPTABLES -t filter -P FORWARD DROP
+$IPTABLES -t filter -A INPUT -i lo -j ACCEPT
+$IPTABLES -t filter -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 $IPTABLES -t filter -A INPUT -p tcp --dport 22 -j ACCEPT
+$IPTABLES -t filter -A INPUT -j REJECT --reject-with icmp-host-prohibited
 /etc/init.d/iptables save
