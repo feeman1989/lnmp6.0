e386ffe70bc17808b1eccd3a9d39b45236dfb650
unknown <YW0463@YW-01B-0590.corp.yaowan.com>
2016-04-11 14:14:30 +0800

    modified:   scripts/iptables.sh

--- scripts/iptables.sh
+++ scripts/iptables.sh
@@ -4,12 +4,11 @@ IPTABLES=`which iptables`
 $IPTABLES -t filter -P INPUT ACCEPT
 $IPTABLES -t filter -P OUTPUT ACCEPT
 $IPTABLES -t filter -P FORWARD ACCEPT
-
 $IPTABLES -t filter -F
 $IPTABLES -t nat -F
 $IPTABLES -t filter -P INPUT DROP
-$IPTABLES -t filter -P OUTPUT ACCEPT
 $IPTABLES -t filter -P FORWARD DROP
+$IPTABLES -t filter -P OUTPUT ACCEPT
 $IPTABLES -t filter -A INPUT -i lo -j ACCEPT
 $IPTABLES -t filter -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 $IPTABLES -t filter -A INPUT -p tcp --dport 22 -j ACCEPT
